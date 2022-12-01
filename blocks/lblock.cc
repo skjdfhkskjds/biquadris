42d638178d60fa32db3198bf70b3d7e5a492574e
dominicmuth03 <dominic.muth03@gmail.com>
2022-11-30 22:21:34 -0500

    update lbock

--- blocks/lblock.cc
+++ blocks/lblock.cc
@@ -1,5 +1,5 @@
 #include "../block.h"
-#include "oblock.h"
+#include "lblock.h"
 #include "../coordinates.h"
 #include <vector>
 #include <memory>
@@ -11,43 +11,39 @@ int down = 1;
 int left = -1;
 int right = 1;
 // maps clockwise states
-map<int, vector<vector<int>>> OBlock::rotationStates ={
-    {0, {{0, up}, {0, up}, {0, up}, {0, up}}},
-    {1, {{right, 0}, {right, 0}, {right, 0}, {right, 0}}},
-    {2, {{0, down}, {0, down}, {0, down}, {0, down}}}, 
-    {3, {{left, 0}, {left, 0}, {left, 0}, {left, 0}}}
+map<int, vector<vector<int>>> LBlock::rotationStates ={
+    {0, {{0, up*2}, {left, up}, {left*2, 0}, {left, down}}},
+    {1, {{right*2, 0}, {right, up}, {0, up*2}, {left, up}}},
+    {2, {{0, down*2}, {right, down}, {right*2, 0}, {right, up}}}, 
+    {3, {{left*2, 0}, {left, down}, {0, down*2}, {right, down}}}
 };
 
-OBlock::OBlock() : Block{} {
+LBlock::LBlock() : Block{} {
     vector<Coordinates> coords;
-    Coordinates a(0,2);
-    Coordinates b(1,2);
-    Coordinates c(0,3);
-    Coordinates d(1,3);
+    Coordinates a(3,0);
+    Coordinates b(3,1);
+    Coordinates c(3,2);
+    Coordinates d(2,2);
     coords.emplace_back(a);
     coords.emplace_back(b);
     coords.emplace_back(c);
     coords.emplace_back(d); 
-    setChar('O');
+    setChar('L');
     setCoords(coords);
 }
-void OBlock::rotateClockwise() //do nothing
+void LBlock::rotateClockwise() 
 {
     //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
-    /*
     int r = getState() % 4;
     updateCoords(rotationStates[r]);
     setState(getState() + 1);
-    */
 }
 
-void OBlock::rotateCounterClockwise() //do nothing
+void LBlock::rotateCounterClockwise() 
 {
     //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
     // 3 -> 2, 2 -> 1, 1 -> 0, 0 -> 3
-    /*
     int r = (getState() + 3) % 4;
     updateCoords(rotationStates[r]);
     setState(getState() - 1);
-    */
 }
