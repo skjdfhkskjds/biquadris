fc9184798af17f724ac89989646bcd780699aca1
dominicmuth03 <dominic.muth03@gmail.com>
2022-11-30 23:50:37 -0500

    cum

--- blocks/iblock.h
+++ blocks/iblock.h
@@ -1,10 +1,14 @@
 #ifndef _IBLOCK_
 #define _IBLOCK_
 
+#include <vector>
+#include <map>
+#include <memory>
 #include "../block.h"
 
 class IBlock : public Block
 {
+    static std::map<int, std::vector<std::vector<int>>> rotationStates;
 public:
     IBlock();
     void rotateCounterClockwise() override;
