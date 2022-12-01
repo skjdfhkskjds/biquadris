da95c8694d6d293a78845f08156e4649fb364100
dominicmuth03 <dominic.muth03@gmail.com>
2022-11-30 22:27:48 -0500

    update jblock

---
+++ blocks/jblock.cc
@@ -0,0 +1,49 @@
+#include "../block.h"
+#include "jblock.h"
+#include "../coordinates.h"
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+int up = -1;
+int down = 1;
+int left = -1;
+int right = 1;
+// maps clockwise states
+map<int, vector<vector<int>>> JBlock::rotationStates ={
+    {0, {{0, up*2}, {left, up}, {left*2, 0}, {right, up}}},
+    {1, {{right*2, 0}, {right, up}, {0, up*2}, {right, down}}},
+    {2, {{0, down*2}, {right, down}, {right*2, 0}, {left, down}}}, 
+    {3, {{left*2, 0}, {left, down}, {0, down*2}, {left, up}}}
+};
+
+JBlock::JBlock() : Block{} {
+    vector<Coordinates> coords;
+    Coordinates a(3,0);
+    Coordinates b(3,1);
+    Coordinates c(3,2);
+    Coordinates d(0,2);
+    coords.emplace_back(a);
+    coords.emplace_back(b);
+    coords.emplace_back(c);
+    coords.emplace_back(d); 
+    setChar('L');
+    setCoords(coords);
+}
+void JBlock::rotateClockwise() 
+{
+    //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
+    int r = getState() % 4;
+    updateCoords(rotationStates[r]);
+    setState(getState() + 1);
+}
+
+void JBlock::rotateCounterClockwise() 
+{
+    //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
+    // 3 -> 2, 2 -> 1, 1 -> 0, 0 -> 3
+    int r = (getState() + 3) % 4;
+    updateCoords(rotationStates[r]);
+    setState(getState() - 1);
+}
--- blocks/jblock.h
+++ blocks/jblock.h
@@ -1,10 +1,14 @@
 #ifndef _JBLOCK_
 #define _JBLOCK_
 
+#include <vector>
+#include <map>
+#include <memory>
 #include "../block.h"
 
 class JBlock : public Block
 {
+    static std::map<int, std::vector<std::vector<int>>> rotationStates;
 public:
     JBlock();
     void rotateCounterClockwise() override;
