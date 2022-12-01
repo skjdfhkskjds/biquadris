5fc66954a462460a4157412c97a6f2e00ee8c294
dominicmuth03 <dominic.muth03@gmail.com>
2022-11-30 22:07:12 -0500

    update oblock

---
+++ blocks/oblock.cc
@@ -0,0 +1,49 @@
+#include "../block.h"
+#include "oblock.h"
+#include "../coordinates.h"
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+int up = -1;
+int down = 1;
+int left = -1;
+int right = 1;
+// maps clockwise states
+map<int, vector<vector<int>>> OBlock::rotationStates ={
+    {0, {{0,up}, {0, up}, {0, up}, {0, up}}},
+    {1, {{right, 0}, {right, 0}, {right, 0}, {right, 0}}},
+    {2, {{0, down}, {0, down}, {0, down}, {0, down}}}, 
+    {3, {{left, 0}, {left, 0}, {left, 0}, {left, 0}}}
+};
+
+OBlock::OBlock() : Block{} {
+    vector<Coordinates> coords;
+    Coordinates a(0,2);
+    Coordinates b(1,2);
+    Coordinates c(0,3);
+    Coordinates d(1,3);
+    coords.emplace_back(a);
+    coords.emplace_back(b);
+    coords.emplace_back(c);
+    coords.emplace_back(d); 
+    setChar('T');
+    setCoords(coords);
+}
+void OBlock::rotateClockwise() 
+{
+    //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
+    int r = getState() % 4;
+    updateCoords(rotationStates[r]);
+    setState(getState() + 1);
+}
+
+void OBlock::rotateCounterClockwise() 
+{
+    //IMPLEMENT A COLLISION CHECK METHOD AND CALL IT BEFORE UPDATECOORDS
+    // 3 -> 2, 2 -> 1, 1 -> 0, 0 -> 3
+    int r = (getState() + 3) % 4;
+    updateCoords(rotationStates[r]);
+    setState(getState() - 1);
+}
--- blocks/oblock.h
+++ blocks/oblock.h
@@ -1,10 +1,14 @@
 #ifndef _OBLOCK_
 #define _OBLOCK_
 
+#include <vector>
+#include <map>
+#include <memory>
 #include "../block.h"
 
 class OBlock : public Block
 {
+    static std::map<int, std::vector<std::vector<int>>> rotationStates;
 public:
     OBlock();
     void rotateCounterClockwise() override;
