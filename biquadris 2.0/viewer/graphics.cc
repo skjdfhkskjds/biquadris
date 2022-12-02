51e970f20e172f7b1b089ac82a48467b16526e05
dominicmuth03 <dominic.muth03@gmail.com>
2022-12-02 13:11:12 -0500

    cum

---
+++ biquadris 2.0/viewer/graphics.cc
@@ -0,0 +1,43 @@
+#include "graphics.h"
+#include "../model/player.h"
+#include "../model/board.h"
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <map>
+using namespace std;
+const int width = 11;
+const int height = 18;
+
+/*
+class Graphics : public Observer
+{
+    std::vector<std::shared_ptr<Player>> players;
+    std::vector<std::vector<char>> oldStates;
+    std::vector<std::vector<char>> newStates;
+public:
+    Graphics(std::vector<std::shared_ptr<Player>> players);
+    ~Graphics();
+    void notify() override;
+};
+*/
+
+//returns a map of index to char, representing all the differences between a player's old boardstate to it's new boardstate.
+map<int,char> Graphics::getDeltas(int p) {  // takes int p, 0 for player1, 1 for player2
+    map<int,char> deltas;
+    for(int i = 0; i < width * height; i++) {
+        char curr = newStates[p][i]''
+        if (curr != oldStates[p][i]) {
+            map[i] = curr;
+        }
+    }
+}
+
+Graphics::Graphics(vector<shared_ptr<Player>> players) : players{players} {
+    newStates[0] = players[0]->getState();
+    newStates[1] = players[1]->getState();
+    for(int i = 0; i < width * height; i++) {
+        oldStates[0][i] = '~';;
+        oldStates[1][i] = '~';
+    }
+}
