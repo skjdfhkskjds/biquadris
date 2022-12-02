54f899b6c9a746e9349a549af0d53af01b9457e0
dominicmuth03 <dominic.muth03@gmail.com>
2022-12-01 20:56:04 -0500

    g

--- biquadris 2.0/controller/main.cc
+++ biquadris 2.0/controller/main.cc
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
 int main(int argc, char** argv)
 {
+    for (int i = 0; i < argc; i++) {
+        if (argv[i] == "-text") {
+
+        } else if (argv[i] == "-seed") {
+            i++;
+            string seed = argv[i];
+            
+        } else if (argv[i] == "-scriptfile") {
+            i++;
+            ifstream file{argv[i]};
+            
+        } else if(argv[i] == "-scriptfile2") {
+            i++;
+            ifstream file{argv[i]};
+            
+        } else if (argv[i] == "-startlevel") {
+            i++;
+            int level = stoi(argv[i]);
+        } else { //show text and graphics
+
+        }
+    }
 
 }
