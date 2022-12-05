70b79a2d6b0516f1ff6f28f18c1a89737eff0664
skjdfhkskjds <colin.chuu@gmail.com>
2022-12-05 12:38:46 -0500

    boardstate change

--- biquadris3/model/effects/force.cc
+++ biquadris3/model/effects/force.cc
@@ -22,7 +22,6 @@ void Force::setBlock(shared_ptr<Block> &block) { component->setBlock(block); }
 
 void Force::setLevel(int level) { component->setLevel(level); }
 
-
 void Force::left() { component->left(); }
 
 void Force::right() { component->right(); }
